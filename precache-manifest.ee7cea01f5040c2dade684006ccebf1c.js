fb3c615b3b1261a748e0284411cafa159ee8e540
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-23 08:59:19 +0000

    Deploy nagesh313/lush-delightful-decor to github.com/nagesh313/lush-delightful-decor.git:gh-pages

---
+++ precache-manifest.ee7cea01f5040c2dade684006ccebf1c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48fd959240e64c7729ec82b465de339",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b66c288afdd30878f13c",
+    "url": "./static/css/2.56a2a926.chunk.css"
+  },
+  {
+    "revision": "60b710f5e49adc1eb5f2",
+    "url": "./static/css/main.53ac928e.chunk.css"
+  },
+  {
+    "revision": "b66c288afdd30878f13c",
+    "url": "./static/js/2.e193b482.chunk.js"
+  },
+  {
+    "revision": "cf18f0e49d3ababbf9b4f7a2277edc0e",
+    "url": "./static/js/2.e193b482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60b710f5e49adc1eb5f2",
+    "url": "./static/js/main.b01b9ad4.chunk.js"
+  },
+  {
+    "revision": "2f7361b376f7c0af02df",
+    "url": "./static/js/runtime-main.bd0fb421.js"
+  },
+  {
+    "revision": "c0f1e4a4fdfb8048c72e86aadb2a247d",
+    "url": "./static/media/Roboto-Bold.c0f1e4a4.woff2"
+  },
+  {
+    "revision": "eed9aab5449cc9c8430d7d258108f602",
+    "url": "./static/media/Roboto-Bold.eed9aab5.woff"
+  },
+  {
+    "revision": "3c37aa69cd77e6a53a067170fa8fe2e9",
+    "url": "./static/media/Roboto-Light.3c37aa69.woff2"
+  },
+  {
+    "revision": "ea36cd9a0e9eee97012a67b8a4570d7b",
+    "url": "./static/media/Roboto-Light.ea36cd9a.woff"
+  },
+  {
+    "revision": "1561b424aaef2f704bbd89155b3ce514",
+    "url": "./static/media/Roboto-Medium.1561b424.woff2"
+  },
+  {
+    "revision": "cf4d60bc0b1d4b2314085919a00e1724",
+    "url": "./static/media/Roboto-Medium.cf4d60bc.woff"
+  },
+  {
+    "revision": "3cf6adf61054c328b1b0ddcd8f9ce24d",
+    "url": "./static/media/Roboto-Regular.3cf6adf6.woff"
+  },
+  {
+    "revision": "5136cbe62a63604402f2fedb97f246f8",
+    "url": "./static/media/Roboto-Regular.5136cbe6.woff2"
+  },
+  {
+    "revision": "1f35e6a11d27d2e10d28946d42332dc5",
+    "url": "./static/media/Roboto-Thin.1f35e6a1.woff2"
+  },
+  {
+    "revision": "44b78f142603eb69f593ed4002ed7a4a",
+    "url": "./static/media/Roboto-Thin.44b78f14.woff"
+  }
+]);
